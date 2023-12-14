b08d7a9d7766be907f6f9bb977ca903774e45091
OgataiKhan <badellino.kevin@gmail.com>
2023-12-14 14:20:46 +0100

    feat: additional scaffolding & axios

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <script>
+import axios from 'axios';
+import { store } from './store';
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+
 export default {
   name: 'DefaultTemplate',
-
+  components: {
+    Header,
+    Main,
+  },
   data() {
     return {
-      text: 'Template VueJS',
+      store,
     };
   },
 };
 </script>
 
 <template>
-  <h1>{{ text }}</h1>
+  <Header />
+  <Main />
+  <!-- <h1>{{ text }}</h1>
   <p><font-awesome-icon icon="fa-solid fa-star" /></p>
-  <p><font-awesome-icon icon="fa-regular fa-star" /></p>
+  <p><font-awesome-icon icon="fa-regular fa-star" /></p> -->
 </template>
 
 <style scoped lang="scss"></style>
